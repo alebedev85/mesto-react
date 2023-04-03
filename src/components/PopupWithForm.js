fb7e8b06f78e218fcdae80483f18a2324e1c3cde
Lebedev Andrey <alebedev424@gmail.com>
2023-04-03 19:41:34 +0300

    refactor: исправил футер и пропсы в попап компаненте

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,13 +1,13 @@
-export default function PopupWithForm(props) {
+export default function PopupWithForm({isOpen, onClose, name, title, buttonText, children}) {
   return (
-    <div className={`popup popup_type_${props.name} ${props.isOpen && ('popup_opened')}`} >
+    <div className={`popup popup_type_${name} ${isOpen && ('popup_opened')}`} >
       <div className="popup__container">
-        <button className="popup__close-button" type="button" onClick={props.onClose} />
-        <h2 className="popup__title">{`${props.title}`}</h2>
+        <button className="popup__close-button" type="button" onClick={onClose} />
+        <h2 className="popup__title">{`${title}`}</h2>
         <form className="popup__form" name="formEditProfile" >
           <fieldset className="popup__fieldset">
-            {props.children}
-            <button className="popup__save-button" type="submit" name="saveButton">{props.buttonText}</button>
+            {children}
+            <button className="popup__save-button" type="submit" name="saveButton">{buttonText}</button>
           </fieldset>
         </form>
       </div>
