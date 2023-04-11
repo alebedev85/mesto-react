e6687b9eaff488f6bc826a787c9c97062d021427
Andrey Lebedev <alebedev424@gmail.com>
2023-04-11 15:00:24 +0300

    refactor: попытался убрать иконку Реакт, попытался сделать закрытие попапа по оверлею и esc

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,6 +1,19 @@
 export default function PopupWithForm({isOpen, onClose, name, title, buttonText, children}) {
+
+  function handleEscClose(evt) {
+    console.log('ghghgf')
+    if (evt.key === 'Escape') {
+      onClose();
+    };
+  }
+
+  function handleOverlayClose(evt) {
+    if (evt.target.classList.contains('popup')) {
+      onClose();
+    };
+  }
   return (
-    <div className={`popup popup_type_${name} ${isOpen && ('popup_opened')}`} >
+    <div className={`popup popup_type_${name} ${isOpen && ('popup_opened')}`} onClick = {handleOverlayClose} onKeyDown={handleEscClose} tabIndex={0}>
       <div className="popup__container">
         <button className="popup__close-button" type="button" onClick={onClose} />
         <h2 className="popup__title">{`${title}`}</h2>
