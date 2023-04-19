ebb9a71d9fd91a273db83e8bfd7b9a81902c9d71
Andrey Lebedev <alebedev424.gmail.com>
2023-04-19 20:03:41 +0200

    fix: поправил попап по картинки

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,10 +1,16 @@
-export default function ImagePopup(props) {
+export default function ImagePopup({name, card, onClose}) {
+  function handleOverlayClose(evt) {
+    if (evt.target.classList.contains('popup')) {
+      onClose();
+    };
+  }
   return (
-    <div className={`popup popup_type_${props.name} ${Object.keys(props.card).length && ('popup_opened')}`} >
+    <div className={`popup popup_type_${name} ${Object.keys(card).length && ('popup_opened')}`}
+    onClick={handleOverlayClose} >
       <div className="popup__picture-container">
-        <img className="popup__picture" src={props.card.link} alt={`Фото ${props.card.name}`} />
-        <button className="popup__close-button" type="button" onClick={props.onClose}></button>
-        <h2 className="popup__picture-title">{props.card.name}</h2>
+        <img className="popup__picture" src={card.link} alt={`Фото ${card.name}`} />
+        <button className="popup__close-button" type="button" onClick={onClose}></button>
+        <h2 className="popup__picture-title">{card.name}</h2>
       </div>
     </div>
   )
